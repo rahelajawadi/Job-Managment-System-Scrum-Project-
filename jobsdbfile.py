5900fb10b51db06a3c8c6d2e0a70ebcff5fdb11e
rahajawadi <raha.jawadi@gmail.com>
2021-04-02 09:52:35 +0430

    Database file Added for the Tables.

--- jobsdbfile.py
+++ jobsdbfile.py
@@ -5,24 +5,17 @@ def createDB():
     # creating the connection
     db = sqlite3.connect("jobs.db")
     #create table
-    db.execute("create table userReg(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
-    "name, password )")
+    #db.execute("create table userReg(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+    #"name, password )")
+
 
     # insert values
-    #db.execute('drop table jobInfo')
-    # db.execute( "INSERT INTO userReg(name, password) "
-    # "VALUES('Rahela', 'rahelaraha', '12345', 'rahela.jawadi@gmail.com', '0780092896','male');")
-
-    #db.execute("create table jobInfo(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
-    #"title text, organization text ,details text, open text, close text)")
-
-    # insert values
-    #db.execute( "INSERT INTO jobInfo (title, organization,details, open, close) "
-    #"VALUES('Software Enginier ', 'SYPA', 'Bachelor Degree', '6/30/2020', '7/30.2020');")
+    db.execute( "INSERT INTO userReg (name, password) "
+    "VALUES('Rahela', '12345');")
 
     db.commit()
 
-    cursor = db.execute("select * from jobInfo")
+    cursor = db.execute("select * from userReg")
     for i in cursor:
         print(i)
 
