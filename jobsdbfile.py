951a148eab81bfbaa6b296567f9b9c71732d3f49
raha.jawadi <raha.jawadi@gmail.com>
2021-04-14 11:33:24 +0430

    Registration Form created and connected to Database.

--- jobsdbfile.py
+++ jobsdbfile.py
@@ -4,21 +4,28 @@ import sqlite3
 def createDB():
     # creating the connection
     db = sqlite3.connect("jobs.db")
+
+    #Creating Regestration Table
+    #db.execute("create table RegTable(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+     #          "name text, username text ,password text, email text,phone text, gender text)")
+
+    db.execute("INSERT INTO RegTable(name, username, password,email, phone, gender) "
+               "VALUES('Rahela', 'rahelaraha', '12345', 'rahela.jawadi@gmail.com', '0780092896','male');")
     #create table
     #db.execute("create table userReg(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
     #"name, password )")
 
-    db.execute("create table AdLogin(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
-    "name, password )")
+    #db.execute("create table AdLogin(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+    #"name, password )")
 
 
     # insert values
-    db.execute( "INSERT INTO AdLogin (name, password) "
-    "VALUES('Admin', '12345');")
+    #db.execute( "INSERT INTO AdLogin (name, password) "
+    #"VALUES('Admin', '12345');")
 
     db.commit()
 
-    cursor = db.execute("select * from AdLogin")
+    cursor = db.execute("select * from RegTable")
     for i in cursor:
         print(i)
 
